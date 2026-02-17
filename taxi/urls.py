9e55fc8c9f7dc67d61a66c87db82d21e573427ff
nazarkravets2007 <nazarfootballer11@gmail.com>
2026-02-17 17:26:12 +0200

    Add urls for class-based views
    Add urls for class-based views

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
-from .views import index
-
-urlpatterns = [
-    path("", index, name="index"),
-]
+from taxi import views
 
 app_name = "taxi"
+
+urlpatterns = [
+    path("manufacturers/", views.ManufacturerListView.as_view(), name="manufacturer-list"),
+    path("cars/", views.CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", views.CarDetailView.as_view(), name="car-detail"),
+    path("drivers/", views.DriverListView.as_view(), name="driver-list"),
+    path("drivers/<int:pk>/", views.DriverDetailView.as_view(), name="driver-detail"),
+]
